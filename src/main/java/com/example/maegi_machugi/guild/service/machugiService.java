8c335b6abf86a99b12d1c9eeeabdd936cf37d81f
SystemUi <nick102030@naver.com>
2025-04-09 16:31:40 +0900

    [test]: 2차 git action 테스트

--- src/main/java/com/example/maegi_machugi/guild/service/machugiService.java
+++ src/main/java/com/example/maegi_machugi/guild/service/machugiService.java
@@ -20,7 +20,7 @@ import java.util.*;
 @Service
 public class machugiService {
 
-    //git action 테스트
+    //git action 테스트2
     private static final String API_KEY = System.getenv("API_KEY");
     private static WebClient webClient;
 
